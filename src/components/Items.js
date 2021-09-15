2562c37f93b220c23c7e77ab30b9d548eecd2dcd
Mihika Parmar <80045642+mihikaparmar@users.noreply.github.com>
2021-09-15 21:26:13 +0530

    Changes

---
+++ src/components/Items.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import "./App.css";
+
+
+export class Img extends Component {
+    render() { 
+        return (
+            
+            <>
+            <div className="container-fluid my-5 mx-5 col-8 kg">
+                
+           <div className="card  " style={{height:"450px",width:"250px",border:"2px solid whitte",borderRadius:"20px"}}>
+  <img src="https://source.unsplash.com/random" className="card-img-top flex-sm-row " alt="..."   style={{height:"700px",width:"250px",border:"2px solid whitte",borderRadius:"20px"}}/>
+  
+  
+</div>
+<p class="card-text">HEY..</p>
+</div>
+            
+            </>
+
+        )
+    }
+    }
+
+    export default Img
